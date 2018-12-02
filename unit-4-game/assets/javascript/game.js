bc71f0c3f8d88382936d0a5dae37184176d5938a
Juan Duran <44187573+juanduran23@users.noreply.github.com>
2018-12-02 00:01:03 -0500

    Add files via upload

---
+++ unit-4-game/assets/javascript/game.js
@@ -0,0 +1,14 @@
+var target = " ";
+var wins = 0;
+var loses = 0;
+var userScore = 0;
+
+
+var crystals = ["assets/images/images-1.jpg","assets/images/images-2.jpg","assets/images/images-3.jpg","assets/images/images.jpg"]
+
+
+function randomtNumber () {
+    targetNumber = Math.floor(Math.random() * 102) + 19;
+    
+    console.log(randomNumber);
+}
